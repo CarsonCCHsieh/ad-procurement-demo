233555eb65b14f2d85dfcd34622a16b59add67bc
Carson <carson@local>
2026-03-21 03:52:34 +0800

    Remove USADA artifacts from demo repo and mark boundaries

--- usadanews-code-snapshot/ops/run_sync_sheet.py
+++
@@ -1,5 +0,0 @@
-﻿import re, urllib.request
-s=open('vt-maint.php','r',encoding='utf-8',errors='ignore').read()
-key=re.search(r'\$secret\s*=\s*"([^"]+)"',s).group(1)
-url=f'https://usadanews.com/vt-maint.php?action=sync_sheet_force&key={key}'
-print(urllib.request.urlopen(url, timeout=120).read().decode('utf-8-sig','ignore'))
